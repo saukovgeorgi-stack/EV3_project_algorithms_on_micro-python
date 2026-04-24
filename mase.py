2fefc309ff4e9599d2783d9e1910552557cf9905
Georgi <saukov.georgi@gmail.com>
2026-04-24 16:38:33 +0300

    A working algorithm with an accelerated PID controller, but without complex checks
    I updated the PID controller and added a function to clear temporary variables. But a new problem has arisen: the robot detects even the slightest crack in the wall, meaning it can turn if it notices a significant change (I’ll add a single-layer check soon to fix this glitch). + I need to calculate the exact turn, so I might add a gyroscope sensor

--- mase.py
+++ mase.py
@@ -30,36 +30,45 @@ def pid(): #simple PID-controller
     global last_error, state, target, dis_r, dis_l
 
     error = dis_r - target
-    if error > last_error + 10 or error < last_error -10:
+    if error > last_error + 30 or error < last_error -30:
         moving()
         print("moving_pid")
-        motor_l.run_angle(600, 250, wait=False) #(speed, angle, wait)
-        motor_r.run_angle(600, -250, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
+        motor_l.run_angle(600, 350, wait=False) #(speed, angle, wait)
+        motor_r.run_angle(600, -350, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
         print("right turning")
         moving()
         print("moving_pid")
-        target = 0
-        state = 1 #this is our break, because utside the loop we can't use simple break
+        rollback()
     else:
-        motor_r.run(target)
-        motor_l.run(target)
+        motor_r.run(-error*5)
+        motor_l.run(error*5)
         print("pid")
     last_error = error
+
+def rollback(): #function to reset all temporary variables 
+    global target, error, last_error
+
+    target = 0
+    error = 0
+    last_error = 0
+
 last_error = 0
 target = 0
 error = 0
+
 state = 0
 
 while True:
     dis_f, dis_r, dis_l = get_distances() #this function is not optimised, but now it's OK
 
-    if state == 1:
-        break
-    elif dis_f < 150: #distance is in mm so it's 15 cm
-        motor_l.run_angle(600, -250, wait=False) #(speed, angle, wait)
-        motor_r.run_angle(600, 250, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
+    if dis_f < 150: #distance is in mm so it's 15 cm
+        motor_l.run_angle(400, -350, wait=False) #(speed, angle, wait)
+        motor_r.run_angle(400, 350, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
         print("left turning")
-        break
+        wait(10)
+        motor_l.stop()
+        motor_r.stop()
+        rollback()
     elif target == 0:
         target = dis_r
     elif dis_r < target-2 or dis_r > target+2:
@@ -70,4 +79,3 @@ while True:
         print("moving")
 
     wait(10) #wait for high-quality cycle processing
-print(target)
