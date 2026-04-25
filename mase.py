ce6e26ec61cd241439aefc79e2aa88b5e2d97350
Georgi <saukov.georgi@gmail.com>
2026-04-25 08:11:28 +0300

    Added error checking (2550) for ultrasonic sensors. Improved the PID controller, increased the robot's speed, and made the code more readable.
    Refactor PID controller logic and improve distance handling. All that's left is to finalize the PID controller, optimize the cycle time, clean up the code, create an index map using tuples, and add Bluetooth for remote operation and log reading.

--- mase.py
+++ mase.py
@@ -14,68 +14,79 @@ us_r = UltrasonicSensor(Port.S4) #sensors
 us_l = UltrasonicSensor(Port.S2)
 
 def get_distances():
+    
     dis_f = us_f.distance()
     dis_r = us_r.distance()
     dis_l = us_l.distance()
+
+    if dis_f == 2550: dis_f = 9999
+    if dis_r == 2550: dis_r = 9999
+    if dis_l == 2550: dis_l = 9999
     return dis_f, dis_r, dis_l
 
 def moving():
     motor_r.run_angle(500, 360, wait=False)
-    motor_l.run_angle(500,360, wait=True)
-    wait(10)
+    motor_l.run_angle(500, 360, wait=True)
     motor_l.stop()
     motor_r.stop()
 
 def pid(): #simple PID-controller
-    global last_error, state, target, dis_r, dis_l
+    global last_error, error, attempt_count, state, target, dis_r, dis_l
 
     error = dis_r - target
-    if error > last_error + 30 or error < last_error -30:
-        moving()
-        print("moving_pid")
-        motor_l.run_angle(600, 350, wait=False) #(speed, angle, wait)
-        motor_r.run_angle(600, -350, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
-        print("right turning")
-        moving()
-        print("moving_pid")
-        rollback()
+    if error > last_error + 15 or error < last_error -15 or error > 100:
+        if attempt_count > 1:
+            moving()
+            wait(10)
+            print("moving_pid")
+            motor_l.run_angle(600, 260, wait=False) #(speed, angle, wait)
+            motor_r.run_angle(600, -260, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
+            print("right turning")
+            moving()
+            print("moving_pid")
+            rollback()
+            wait(10)
+        else:
+            attempt_count += 1
     else:
-        motor_r.run(-error*5)
-        motor_l.run(error*5)
-        print("pid")
+        attempt_count = 0 #attempt_count reset: we don't use rollback because can reset all settings
+
+        motor_r.run(400-(error*4))
+        motor_l.run(400+(error*4))
+        print("pid", attempt_count)
     last_error = error
 
 def rollback(): #function to reset all temporary variables 
-    global target, error, last_error
+    global target, error, last_error, attempt_count
 
     target = 0
     error = 0
     last_error = 0
+    attempt_count = 0
 
 last_error = 0
 target = 0
 error = 0
+
+attempt_count = 0
 
 state = 0
 
 while True:
     dis_f, dis_r, dis_l = get_distances() #this function is not optimised, but now it's OK
 
-    if dis_f < 150: #distance is in mm so it's 15 cm
-        motor_l.run_angle(400, -350, wait=False) #(speed, angle, wait)
-        motor_r.run_angle(400, 350, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
+    if dis_f <= 100: #distance is in mm so it's 10 cm
+        motor_l.run_angle(400, -300, wait=False) #(speed, angle, wait)
+        motor_r.run_angle(400, 300, wait=True) #the turning angle depends on your wheels, so choose it to suit yourself
         print("left turning")
         wait(10)
         motor_l.stop()
         motor_r.stop()
+        wait(10)
         rollback()
     elif target == 0:
         target = dis_r
-    elif dis_r < target-2 or dis_r > target+2:
-        pid()
     else:
-        motor_r.run(500)
-        motor_l.run(500)
-        print("moving")
+        pid()
 
     wait(10) #wait for high-quality cycle processing
